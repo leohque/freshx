af4a13e2881ee5942a52880c89c668a147505a90
leo martinez <leohque@gmail.com>
2020-03-04 18:11:25 -0300

    Added Devise and created Controller and Views for User, set up new rails application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Devise config
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
+
+
