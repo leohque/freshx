e9c4855a2adf68f6a38cda3c0796c2cf47d77536
Ivan Lozano <ivanlozmar@gmail.com>
2020-03-05 18:29:09 -0300

    Created routes and controller for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :posts do
+    resources :comments, only: :create
+  end
+
+  resources :comments, only: [:update, :destroy]
+
 
 end
