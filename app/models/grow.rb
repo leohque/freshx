5f5dfbb1f08f67140bca823626eaba518e7bd01b
leo martinez <leohque@gmail.com>
2020-03-09 17:12:28 -0300

    Added geolocation to Grow

--- app/models/grow.rb
+++ app/models/grow.rb
@@ -10,4 +10,8 @@ class Grow < ApplicationRecord
 
   validates :name, presence: true
 
+  validates :location, presence: true
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
 end
