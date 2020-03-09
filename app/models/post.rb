5d8c9d32abd659d621662b2ab007e4ad91bfa29d
Ivan Lozano <ivanlozmar@gmail.com>
2020-03-09 12:20:43 -0300

    add grow and plant to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  belongs_to :plant, optional: true
+  belongs_to :grow, optional: true
   has_many :hashtag_posts, dependent: :destroy
   has_many :hashtags, through: :hashtag_posts
   has_many :comments, dependent: :destroy
