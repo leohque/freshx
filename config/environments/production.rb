67ab7f6d72ad084348584e0afabad2eafa6753c3
leo martinez <leohque@gmail.com>
2020-03-10 15:35:40 -0300

    Heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Devise
+  config.action_mailer.default_url_options = { host: 'https://freshx.herokuapp.com/' }
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -27,7 +31,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
