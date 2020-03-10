e35581bb5c9b6323b512a23f3c4b7c79c14d8496
Ivan Lozano <ivanlozmar@gmail.com>
2020-03-10 17:29:27 -0300

    add create hashtags to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,17 @@ class Post < ApplicationRecord
   # validates :photos, attached: true, content_type: [:png, :jpg, :jpeg]
 
   validates :content, presence: true
+
+  after_commit :create_hashtags
+
+  def extract_name_hashtags
+    content.to_s.scan(/#\w+/).map{|name| name.gsub("#", "")}
+  end
+
+  def create_hashtags
+    extract_name_hashtags.each do |name|
+      Hashtag.create(name: name)
+    end
+  end
+
 end
