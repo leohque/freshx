37b13cd974d8831314e57ddd7d053c877b572df6
Ivan Lozano <ivanlozmar@gmail.com>
2020-03-10 15:09:34 -0300

    added heart fucntionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,33 @@
 class LikesController < ApplicationController
+  def index
+    # @likes = Like.where(user: current_user)
+    @likes = current_user.likes.order(created_at: :desc)
+    # @likes = policy_scope(Order).order(created_at: :desc) # -> this would give us the same result
+
+  end
+
   def create
     @like = Like.new
     @like.user = current_user
     @like.post = Post.find(params[:post_id])
     @like.save
 
-    # respond_to do |format|
-    #     format.html { redirect_to post_path }
-    #     format.js  # <-- will render `app/views/likes/create.js.erb`
-    # end
+    respond_to do |format|
+      format.html { redirect_to post_path(@like.post) }
+      format.js  # <-- will render `app/views/likes/create.js.erb`
+    end
   end
 
   def destroy
     @like = Like.find(params[:id])
+
+    @post_id = @like.post_id
+
     @like.destroy
+
+    respond_to do |format|
+      format.html { redirect_to post_path(@like.post) }
+      format.js  # <-- will render `app/views/likes/create.js.erb`
+    end
   end
 end
