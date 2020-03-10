1c29eb47c3c3efe365be28c45012705f742697f5
leo martinez <leohque@gmail.com>
2020-03-10 11:37:48 -0300

    Fixed map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,9 +9,16 @@ class PagesController < ApplicationController
     @users = User.geocoded #returns users with coordinates
 
     @markers = @users.map do |user|
-      {
-        lat: user.latitude,
-        lng: user.longitude
+      if user.photo.attached?
+        user_avatar = Cloudinary::Utils.cloudinary_url(user.photo.key, {})
+      else
+        user_avatar = 'anon.jpg'
+      end
+       {
+        lat: user.latitude + rand(-0.02..0.02),
+        lng: user.longitude + rand(-0.02..0.02),
+        infoWindow: render_to_string(partial: "users/info_window", locals: { user: user }),
+        image_url: helpers.asset_url(user_avatar)
       }
     end
   end
